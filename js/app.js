14b0e5deba5f9977a2122ac1fe9b082ee0a3f2b6
Stian Westvig <stian.westvig@bouvet.no>
2014-04-30 14:41:53 +0200

    tried out some animation of carousel. Decided to do more later.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['mm.foundation', 'ngRoute']);
+var app = angular.module('app', ['mm.foundation', 'ngRoute', 'ngAnimate']);
 
 
 app.config(function($routeProvider, $locationProvider){

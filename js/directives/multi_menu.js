eac3891fc97d1f699cb4d3a1821b2252afcf2192
Adam Haeger <adam.haeger@bouvet.no>
2014-05-08 16:24:53 +0200

    can kind of move the menu around

--- js/directives/multi_menu.js
+++ js/directives/multi_menu.js
@@ -1,30 +1,22 @@
-/*myApp.run(function($templateCache) {
-    $templateCache.put('templateId.html', 'This is the content of the template');
-});*/
-
-/*
-angular.module("menu_item.html", []).run(["$templateCache", function($templateCache) {
-    $templateCache.put("menu_item.html",
-    "<ul><li ng-repeat=\"item in item.categories\" ng-include=\"'menu_item.html'\"></li></ul>");
-}]);
-*/
-
-
-
 angular.module('agh.multimenu', [])
 
     .controller('MenuController', ['$scope', '$attrs', function($scope, $attrs) {
+
         var menu = this;
-
         $scope.activeLevel = 1;
-
 
         menu.init = function(menuItems){
             $scope.menuItems = menuItems;
         }
 
-        menu.setActiveLevel = function(level){
-            $scope.activeLevel = level;
+        menu.incrementActiveLevel = function(){
+            $scope.activeLevel = $scope.activeLevel + 1;
+            console.log($scope.activeLevel);
+        }
+
+        menu.decrementActiveLevel = function(){
+            $scope.activeLevel = $scope.activeLevel - 1;
+            console.log($scope.activeLevel);
         }
 
     }])
@@ -46,25 +38,30 @@ angular.module('agh.multimenu', [])
             }
         };
     })
-/*
-
 
     .directive( 'menuitem', function () {
         return {
             restrict: 'EA',
             replace: true,
-            templateUrl: 'views/multimenu.html',
-            scope: {
-                menuitems: '='
-            },
-            controller: 'MenuController',
+            require: '^multimenu',
             link: function(scope, element, attrs, ctrls) {
-                var menuCtrl = ctrls[0];
-                if ( menuCtrl) {
-                    menuCtrl.init(scope.menuitems);
-                }
+                var menuCtrl = ctrls;
+                element.bind('click', function(){
+                    menuCtrl.incrementActiveLevel();
+                })
             }
         };
     })
-*/
 
+    .directive( 'backbutton', function () {
+        return {
+            restrict: 'EA',
+            require: '^multimenu',
+            link: function(scope, element, attrs, ctrls) {
+                var menuCtrl = ctrls;
+                element.bind('click', function(){
+                    menuCtrl.decrementActiveLevel();
+                })
+            }
+        };
+    })

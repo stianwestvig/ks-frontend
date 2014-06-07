65a99e978f925fdb54a74f489eef224b46edb962
Stian Westvig <stian.westvig@bouvet.no>
2014-06-07 17:50:30 +0200

    added a new grunt task: to replace string paths in the new baked_server.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,41 +15,86 @@ module.exports = function(grunt) {
         }        
       }
     },
-      uglify: {
-          js: {
-              options: {
-                  mangle: false,
-                  compress: false
-              },
-              files: {
-                  'dist/baked.js': [
-                      'bower_components/modernizr/modernizr.js',
-                      'bower_components/angular/angular.min.js',
-                      'bower_components/angular-animate/angular-animate.js',
-                      'bower_components/angular-foundation/mm-foundation.js',
-                      'bower_components/angular-sanitize/angular-sanitize.min.js',
-                      'bower_components/angular-route/angular-route.min.js',
-                      'bower_components/angular-bootstrap/ui-bootstrap.min.js',
-                      'bower_components/ngLocale/angular-locale_no.js',
-                      'bower_components/fastclick/lib/fastclick.js',
-                      'js/vendor/ui-bootstrap-custom/datepicker.js',
-                      'js/app.js',
-                      'js/controllers/navigation_controller.js',
-                      'js/controllers/slider_controller.js',
-                      'js/controllers/status_update_controller.js',
-                      'js/controllers/calendar_controller.js',
-                      'js/directives/slider.js',
-                      'js/directives/status_update.js',
-                      'js/directives/tooltip.js',
-                      'js/directives/multi_menu.js',
-                      'js/filters/date_filter.js',
-                      'js/services/helper_service.js',
-                      'js/services/data_service.js',
-                      'js/services/async_data_service.js'
-                  ]
-              }
+    uglify: {
+      js: {
+          options: {
+              mangle: false,
+              compress: false
+          },
+          files: {
+              'dist/baked.js': [
+                  'bower_components/modernizr/modernizr.js',
+                  'bower_components/angular/angular.min.js',
+                  'bower_components/angular-animate/angular-animate.js',
+                  'bower_components/angular-foundation/mm-foundation.js',
+                  'bower_components/angular-sanitize/angular-sanitize.min.js',
+                  'bower_components/angular-route/angular-route.min.js',
+                  'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+                  'bower_components/ngLocale/angular-locale_no.js',
+                  'bower_components/fastclick/lib/fastclick.js',
+                  'js/vendor/ui-bootstrap-custom/datepicker.js',
+                  'js/app.js',
+                  'js/controllers/navigation_controller.js',
+                  'js/controllers/slider_controller.js',
+                  'js/controllers/status_update_controller.js',
+                  'js/controllers/calendar_controller.js',
+                  'js/directives/slider.js',
+                  'js/directives/status_update.js',
+                  'js/directives/tooltip.js',
+                  'js/directives/multi_menu.js',
+                  'js/filters/date_filter.js',
+                  'js/services/helper_service.js',
+                  'js/services/data_service.js',
+                  'js/services/async_data_service.js'
+              ]
+              /*'dist/baked_server.js': [
+                  'bower_components/modernizr/modernizr.js',
+                  'bower_components/angular/angular.min.js',
+                  'bower_components/angular-animate/angular-animate.js',
+                  'bower_components/angular-foundation/mm-foundation.js',
+                  'bower_components/angular-sanitize/angular-sanitize.min.js',
+                  'bower_components/angular-route/angular-route.min.js',
+                  'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+                  'bower_components/ngLocale/angular-locale_no.js',
+                  'bower_components/fastclick/lib/fastclick.js',
+                  'js/vendor/ui-bootstrap-custom/datepicker.js',
+                  'js/app.js',
+                  'js/controllers/navigation_controller.js',
+                  'js/controllers/slider_controller.js',
+                  'js/controllers/status_update_controller.js',
+                  'js/controllers/calendar_controller.js',
+                  'js/directives/slider.js',
+                  'js/directives/status_update.js',
+                  'js/directives/tooltip.js',
+                  'js/directives/multi_menu.js',
+                  'js/filters/date_filter.js',
+                  'js/services/helper_service.js',
+                  'js/services/data_service.js',
+                  'js/services/async_data_service.js'
+              ]*/
           }
-      },
+      }
+    },
+
+
+    'string-replace': {
+        files: {
+            src: 'dist/baked.js',
+            dest: 'dist/baked_server.js'
+        },
+        options: {
+            replacements: [
+                {
+                    pattern: 'views/slider.html',
+                    replacement: 'frontend/views/slider.html'
+                },
+                {
+                    pattern: 'views/status_update.html',
+                    replacement: 'frontend/views/status_update.html'
+                }
+            ]
+        }
+    },
 
     watch: {
       grunt: { files: ['Gruntfile.js'] },
@@ -69,7 +114,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-string-replace');
 
-  grunt.registerTask('build', ['sass', 'uglify']);
+  grunt.registerTask('build', ['sass', 'uglify', 'string-replace']);
   grunt.registerTask('default', ['build','watch']);
 }
